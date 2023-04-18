d1571c248c403030f586cabb15d1cce60eec91bf
meschinidino <f.meschini@alumno.um.edu.ar>
2023-04-18 12:27:36 -0300

    tests terminados

--- my_test.py
+++ my_test.py
@@ -9,16 +9,19 @@ class MyTest(unittest.TestCase):
         self.assertEqual(resultado, {"hola": 1,
                                      "mundo": 1
                                      })
+
     def test_complejo(self):
         resultado = contar_palabras("hola hola hola como estas todo bien bien vos bien tambien gracias")
         self.assertEqual(resultado, {"hola": 3,
-                                      "como": 1,
-                                      "estas": 1,
-                                      "todo": 1,
-                                      "bien": 3,
-                                      "vos": 1,
-                                      "tambien": 1,
-                                      "gracias": 1
-                                      })
+                                     "como": 1,
+                                     "estas": 1,
+                                     "todo": 1,
+                                     "bien": 3,
+                                     "vos": 1,
+                                     "tambien": 1,
+                                     "gracias": 1
+                                     })
+
+
 if __name__ == '__main__':
     unittest.main()
